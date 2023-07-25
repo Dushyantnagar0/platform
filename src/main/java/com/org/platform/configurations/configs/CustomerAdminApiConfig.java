a269f1eaa3a5d1d0358ed53f46fcf76fccae0a06
Dushyantnagar0 <dushyantnagar0@gmail.com>
2023-07-25 18:55:41 +0530

    servelt filter optimization for admin and customer apis

---
+++ src/main/java/com/org/platform/configurations/configs/CustomerAdminApiConfig.java
@@ -0,0 +1,23 @@
+package com.org.platform.configurations.configs;
+
+import com.org.platform.configurations.filter.PlatformCustomerAdminApiFilter;
+import com.org.platform.services.interfaces.TokenService;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class CustomerAdminApiConfig {
+
+    @Bean
+    public FilterRegistrationBean<PlatformCustomerAdminApiFilter> filterRegistrationCustomerAdminBean(TokenService tokenService) {
+        FilterRegistrationBean <PlatformCustomerAdminApiFilter> registrationBean = new FilterRegistrationBean();
+        PlatformCustomerAdminApiFilter platformCustomerAdminApiFilter = new PlatformCustomerAdminApiFilter(tokenService);
+
+        registrationBean.setFilter(platformCustomerAdminApiFilter);
+        registrationBean.addUrlPatterns("/consumer/*", "/admin/*");
+        registrationBean.setOrder(1);
+        return registrationBean;
+    }
+
+}
--- src/main/java/com/org/platform/configurations/filter/PlatformCustomerAdminApiFilter.java
+++ src/main/java/com/org/platform/configurations/filter/PlatformCustomerAdminApiFilter.java
@@ -15,23 +15,29 @@ import static com.org.platform.errors.errorCodes.LoginError.INVALID_TOKEN;
 import static com.org.platform.utils.ServletFilterUtils.asHttp;
 import static com.org.platform.utils.ServletFilterUtils.forwardTheApiCall;
 
+
 @Slf4j
 @Component
 @RequiredArgsConstructor
-public class PlatformAdminFilter implements Filter {
+public class PlatformCustomerAdminApiFilter implements Filter {
 
     private final TokenService tokenService;
 
     @Override
+    public void init(FilterConfig filterConfig) throws ServletException {}
+
+    @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         HttpServletRequest httpRequest = asHttp(request);
-        HttpServletResponse httpResponse = asHttp(response);
+
+        boolean isForAdmin = (httpRequest.getRequestURI()).startsWith("/admin");
+
         String tokenId = httpRequest.getHeader("api-token");
         String customerId = httpRequest.getHeader("customerId");
 
-        log.info("http request tokenId : {}", tokenId);
+        log.info("http request isForAdmin and tokenId : {} {}", isForAdmin, tokenId);
         try {
-            if (tokenService.validateJwtTokenAndCreateHeaderMap(httpRequest, tokenId, customerId, true)) {
+            if (tokenService.validateJwtTokenAndCreateHeaderMap(httpRequest, tokenId, customerId, isForAdmin)) {
                 forwardTheApiCall(request, response, chain);
             } else {
                 ((HttpServletResponse) response).setStatus(HttpServletResponse.SC_UNAUTHORIZED);
